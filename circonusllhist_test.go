89791f563a481587f852ab21327075f3b7b81cb4
Theo Schlossnagle <jesus@omniti.com>
2016-09-20 09:18:06 -0400

    Improve worst-case and best-case performance.
    Improve the Compare function performance for normal new bin input path.
    Add a two-level lookup table to speed updates.
    
    Amortized update speed is now 28.5 ns/op using IntScale and no locks.

--- circonusllhist_test.go
+++ circonusllhist_test.go
@@ -82,7 +82,7 @@ func TestDecStrings(t *testing.T) {
 		"H[4.3e-01]=1"}
 	for i, str := range expect {
 		if str != out[i] {
-			t.Errorf(" DecStrign '%v' != '%v'", out[i], str)
+			t.Errorf(" DecString '%v' != '%v'", out[i], str)
 		}
 	}
 }
@@ -120,35 +120,35 @@ func TestQuantiles(t *testing.T) {
 }
 
 func BenchmarkHistogramRecordValue(b *testing.B) {
-	h := hist.New()
-	for i := 0; i < 1000000; i++ {
-		if err := h.RecordValue(float64(i)); err != nil {
-			b.Fatal(err)
-		}
-	}
-	b.ResetTimer()
-	b.ReportAllocs()
-
+	h := hist.NewNoLocks()
 	for i := 0; i < b.N; i++ {
-		h.RecordValue(100)
+		h.RecordValue(float64(i % 1000))
 	}
+	b.ReportAllocs()
 }
 
 func BenchmarkHistogramTypical(b *testing.B) {
-	h := hist.New()
-	for j := 0; j < 1000; j++ {
-		for i := 50; i < 1000; i++ {
-			if err := h.RecordValue(float64(i)); err != nil {
-				b.Fatal(err)
-			}
-		}
-	}
-	b.ResetTimer()
-	b.ReportAllocs()
-
+	h := hist.NewNoLocks()
 	for i := 0; i < b.N; i++ {
-		h.RecordValue(100)
+		h.RecordValue(float64(i % 1000))
 	}
+	b.ReportAllocs()
+}
+
+func BenchmarkHistogramRecordIntScale(b *testing.B) {
+	h := hist.NewNoLocks()
+	for i := 0; i < b.N; i++ {
+		h.RecordIntScale(i%90+10, (i/1000)%3)
+	}
+	b.ReportAllocs()
+}
+
+func BenchmarkHistogramTypicalIntScale(b *testing.B) {
+	h := hist.NewNoLocks()
+	for i := 0; i < b.N; i++ {
+		h.RecordIntScale(i%90+10, (i/1000)%3)
+	}
+	b.ReportAllocs()
 }
 
 func BenchmarkNew(b *testing.B) {
